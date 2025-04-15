62f8cf962af7f3a80b91d389b143fb0f949c7303
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-04-14 22:18:40 -0600

    server file updated

--- routes/labRoutes.mjs
+++ routes/labRoutes.mjs
@@ -7,30 +7,35 @@ const router = express.Router();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// home route
 router.get("/", (req, res) => {
   res.render("home", { title: "Home", message: "Welcome to the home page!" });
 });
 
+// form route
 router.get("/form", (req, res) => {
   res.render("form", { title: "Please fill up the form" });
 });
 
+// after submit route
 router.post("/submit", (req, res) => {
   console.log("Form data:", req.body);
   res.send("Success!");
 });
 
+// image route
 router.get("/image", (req, res) => {
   res.render("image", { title: "If you like it,You can download it!" });
 });
 
 // create route for downloading the image
 // when a GET request is made to '/download' path
+// thanks to the express documentation for hint
 router.get("/download", (req, res) => {
   const filePath = path.join(__dirname, "..", "images", "lab-1.jpg");
   res.download(filePath, "downloaded_lab-1.jpg", (err) => {
     if (err) {
-      console.log("Download error:", err);
+      console.log("Downloading image error:", err);
       res.status(500).send("Download failed.");
     } else {
       console.log("Image downloaded successfully.");
--- server.mjs
+++ server.mjs
@@ -3,25 +3,32 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import bodyParser from "body-parser";
-import router from "./routs/labRoutes.mjs";
+import router from "./routes/labRoutes.mjs";
 
 const app = express();
 const PORT = 3000 || 3001;
 
+// get current directories using import.meta.url
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+// middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, "images")));
+
 app.use(router);
 
+// Routes
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
 
+// run the engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+  console.log(`Server running on port:${PORT}`);
 });
